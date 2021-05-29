9b3420d08f6db174352d0bccf53cc1507e24550b
Paulo Toledo <paulodat.902522195@uniacademia.edu.br>
2021-05-29 15:42:11 -0300

    Criando as validacoes de usuarios

--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,9 +1,20 @@
 module.exports = (app) => {
-    const findAll = () => {
-        return app.db('users').select();
+    const findAll = (filter = []) => {
+        return app.db('users').where(filter).select();
     };
 
-    const save = () => {
+    const save = async (user) => {
+        if(!user.name) return { error: 'O nome do usuário é um atributo obrigatório'};
+        if(!user.registration) return { error: 'A matrícula do usuário é um atributo obrigatório'};
+        if(!user.email) return { error: 'O email do usuário é um atributo obrigatório'};
+        if(!user.passwd) return { error: 'A senha do usuário é um atributo obrigatório'};
+
+        const userReg = await findAll({ registration: user.registration });
+        if(userReg && userReg.length > 0) return { error: 'Existe um usuário com essa matrícula'}
+
+        const userEmail = await findAll({ email: user.email });
+        if(userEmail && userEmail.length > 0) return { error: 'Existe um usuário com esse email'}
+
         return app.db('users').insert(user, '*');
     };
 
