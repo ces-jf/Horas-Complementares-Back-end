d8675ab6a1e8dabfe3ea49dd7b5836817a8c59f0
Paulo Toledo <paulodat.902522195@uniacademia.edu.br>
2021-06-04 07:54:00 -0300

    Configurando a autorizacao

--- ativcompl/src/main/java/br/edu/uniacademia/ativcompl/config/SecurityConfig.java
+++ ativcompl/src/main/java/br/edu/uniacademia/ativcompl/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.edu.uniacademia.ativcompl.security.JWTAuthenticationFilter;
+import br.edu.uniacademia.ativcompl.security.JWTAuthorizationFilter;
 import br.edu.uniacademia.ativcompl.security.JWTUtil;
 
 
@@ -59,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-//		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
